32d393899d84d4c6a3392b1277a2803c9d827730
Ravi Chhetri <ravichhetri@IT001816.com>
2022-02-13 12:53:26 +0530

    MINOR: added about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,16 +12,16 @@ const About = ({
         <UserHeader>About Me</UserHeader>
         <div className="mt-5 px-5">
           <p className="text-xl">I'm <b>Ravi Chhetri</b>, <span className="font-typewriter">Web Developer | Traveller | Foodie</span></p>
-          <p className="text-md mt-5">
+          <p className="text-md mt-5 font-typewriter leading-9">
             {data?.site?.siteMetadata?.intro??''}
           </p>
         </div>
-        <div className="flex px-5 mt-5">
+        {/* <div className="flex px-5 mt-5">
           <div className="w-2/3">
             <StatsGrid/>
           </div>
           <div className="w-1/3">What I do</div>
-        </div>
+        </div> */}
       </div>
     </UserCard>
   )
